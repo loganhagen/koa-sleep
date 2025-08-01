cdabb697ed0efafc1aaab19832aeb9dce4a505a4
loganhagen <loganhagen@gmail.com>
2025-08-01 15:36:13 -0400

    feat: add daily check in component

---
+++ frontend/src/app/components/daily-check-in/DailyCheckIn.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import {
+  Box,
+  Button,
+  Chip,
+  Divider,
+  Paper,
+  Slider,
+  Stack,
+  Typography,
+} from "@mui/material";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
+import CheckIcon from "@mui/icons-material/Check";
+
+const DailyCheckIn = () => {
+  return (
+    <Paper
+      elevation={0}
+      variant="outlined"
+      sx={{
+        p: 4,
+        borderRadius: 10,
+        backgroundColor: "background.paper",
+        textAlign: "left",
+      }}
+    >
+      <Stack spacing={3}>
+        <Stack direction={"row"} spacing={1} justifyContent={"center"}>
+          <Typography
+            variant="h6"
+            component="h3"
+            sx={{ fontWeight: "bold", textAlign: "center" }}
+          >
+            Daily Check-In
+          </Typography>
+          <CheckIcon sx={{ fontSize: 30, color: "green" }} />
+        </Stack>
+
+        <Stack spacing={2} divider={<Divider flexItem />}>
+          <Box>
+            <Typography
+              variant="subtitle1"
+              sx={{ fontWeight: "medium" }}
+              gutterBottom
+            >
+              How are you feeling today?
+            </Typography>
+            <Stack direction="row" spacing={2} alignItems="center">
+              <SentimentVeryDissatisfiedIcon />
+              <Slider
+                aria-label="Feeling"
+                defaultValue={3}
+                step={1}
+                marks
+                min={1}
+                max={5}
+              />
+              <SentimentVerySatisfiedIcon />
+            </Stack>
+          </Box>
+          <Box>
+            <Typography
+              variant="subtitle1"
+              sx={{ fontWeight: "medium" }}
+              gutterBottom
+            >
+              What activities did you engage in yesterday?
+            </Typography>
+            <Stack direction="row" spacing={1} useFlexGap flexWrap="wrap">
+              <Chip label="Late Coffee" variant="outlined" color="warning" />
+              <Chip label="Stressful Day" variant="outlined" color="error" />
+              <Chip label="Meditation" variant="outlined" color="success" />
+              <Chip label="Exercised" variant="outlined" color="primary" />
+              <Chip label="Ate Late" variant="outlined" color="warning" />
+              <Chip label="Good Meal" variant="outlined" color="info" />
+            </Stack>
+          </Box>
+          <Stack direction={"row"} spacing={1} justifyContent={"right"}>
+            <Button variant="contained">Edit</Button>
+            <Button variant="contained">Save</Button>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Paper>
+  );
+};
+
+export default DailyCheckIn;
--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -3,11 +3,13 @@
 import { Box, Stack } from "@mui/material";
 import Header from "../components/header/Header";
 import ActionPlan from "../components/action-plan/ActionPlanCard";
+import DailyCheckIn from "../components/daily-check-in/DailyCheckIn";
 
 const Home = () => {
   return (
     <Stack direction={"column"} spacing={3}>
       <Header />
+      <DailyCheckIn />
       <ActionPlan />
     </Stack>
   );
