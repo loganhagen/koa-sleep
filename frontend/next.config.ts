12785936acfa75f9ade050a61a0e3b36c6ebd9c0
loganhagen <loganhagen@gmail.com>
2025-08-25 14:50:55 -0400

    feat: configure environment-specific .env files

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,19 @@
 import type { NextConfig } from "next";
+import path from "path";
+import dotenv from "dotenv";
+
+// Load environment variables from .env.development or .env.production
+const env = process.env.NODE_ENV || "development";
+const envPath = path.resolve(process.cwd(), `.env.${env}`);
+
+dotenv.config({ path: envPath });
 
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+        destination: `${process.env.EXPRESS_URL}/:path*`,
       },
     ];
   },
