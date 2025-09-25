ae5052cef95b2270ff6e4776914c6b7d6e017094
loganhagen <loganhagen@gmail.com>
2025-09-25 18:06:21 -0400

    fix: move middleware so that it actually works

--- frontend/src/middleware.tsx
+++ frontend/src/middleware.tsx
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const isAuthenticated = request.cookies.get("auth-token")?.value;
-  const protectedRoutes = ["/test"];
+  const protectedRoutes = ["/home", "/history"];
   const isProtectedRoute = protectedRoutes.some((route) =>
     request.nextUrl.pathname.startsWith(route)
   );
