d0f1e5da7364b31d246afe24be3a93bb5e815d72
loganhagen <loganhagen@gmail.com>
2025-09-30 16:17:04 -0400

    fix: data grid displays properly with dark mode

--- frontend/src/app/_components/Sidebar.tsx
+++ frontend/src/app/_components/Sidebar.tsx
@@ -32,7 +32,7 @@ import { useState, useEffect } from "react";
 import { useUser } from "../../providers/userProvider";
 import InfoIcon from "@mui/icons-material/Info";
 import { useLogout } from "@/hooks/useAuth";
-import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+// import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 
 const SidebarComponent: React.FC = () => {
   const pathname = usePathname();
@@ -273,12 +273,12 @@ const SidebarComponent: React.FC = () => {
                   },
                 }}
               >
-                <MuiMenuItem>
+                {/* <MuiMenuItem>
                   <ListItemIcon>
                     <ManageAccountsIcon fontSize="small" />
                   </ListItemIcon>
                   <ListItemText>Account</ListItemText>
-                </MuiMenuItem>
+                </MuiMenuItem> */}
                 <MuiMenuItem
                   onClick={() => {
                     logoutUser();
